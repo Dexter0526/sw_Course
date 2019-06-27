ccb9e691a5cccca49f85905264cbe6d385d1c25c
Dextet0526 <soul9862@naver.com>
2019-06-27 12:41:22 +0900

    no message

---
+++ Java0627/src/com/exam/Ex2.java
@@ -0,0 +1,18 @@
+package com.exam;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		//int a = 10, int b= 20;
+		int a = 10;
+		int b = 20;
+		
+		// 출력문 타입
+		System.out.print("a변수 출력\n");
+		System.out.println("a = " + a);
+		System.out.printf("a = %d\n", a);
+		// %d 정수, %f 실수, %c 문자, %s 문자열
+		
+		System.out.println("a="+a+", b="+b);
+		System.out.printf("a=%d, b=%d\n",a , b);
+	}
+}
