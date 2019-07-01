2aeca065f1395c96ab483f8528d9d12b79be5fda
Dextet0526 <soul9862@naver.com>
2019-07-01 13:49:29 +0900

    조건문

---
+++ Java0701/src/com/exam/Ex5.java
@@ -0,0 +1,19 @@
+package com.exam;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		// 제어문(조건문, 반복문)
+		// 조건문 if switch
+		// 반복문 for while do~while
+		
+		// if(조건식){
+		// 조건이 참일때 실행
+		//	}
+		
+		int a = 7; 
+		if(a < 0) {
+			a = -a;
+		}
+		System.out.println("a = " + a);
+	}
+}
