44c039e9e01ae5036c16f0ad052669d5593eb417
Dextet0526 <soul9862@naver.com>
2019-07-01 15:29:52 +0900

    if 조건문

--- Java0701/src/com/exam/Ex5.java
+++ Java0701/src/com/exam/Ex5.java
@@ -15,5 +15,68 @@ public class Ex5 {
 			a = -a;
 		}
 		System.out.println("a = " + a);
+		
+		a = 6;
+		
+		if(a%2 == 0) {
+			System.out.println("짝수");
+		}else {
+			System.out.println("홀수");
+		}
+		
+		a = 30;
+		int b = 20;
+		int min = 0;
+		System.out.println("최소값 : " + min);
+		
+		//if() {
+		//
+		//}else if() {
+		//	
+		//}else if() {
+		//	
+		//}else {
+		//	
+		//}
+		
+		a = -5;
+		// 양수 영 음수
+		if(a > 0) {
+			System.out.println("양수");
+		}else if(a == 0){
+			System.out.println(a);
+		}else {
+			System.out.println("음수");
+		}
+		
+		int score = 55;
+		// A : 90~100
+		// B : 80~89
+		// C : 70~79
+		// D : 60~69
+		// F : 0~59
+		if(score >= 90) {
+			System.out.println("학점은 A");
+		}else if(score >= 80) {
+			System.out.println("학점은 B");
+		}else if(score >= 70) {
+			System.out.println("학점은 C");
+		}else if(score >= 60) {
+			System.out.println("학점은 D");
+		}else {
+			System.out.println("학점은 F");
+		}
+		
+		// A 90점 이상
+		//		A+ 98~100
+		//		A  94~97
+		//		A- 90~93
+		// B 80점 이상
+		score = 95;
+		String grade = "";
+		if(score >= 90) {
+			
+		}
+		
 	}
 }
