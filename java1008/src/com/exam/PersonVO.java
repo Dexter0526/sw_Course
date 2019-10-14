3b7fd5297b4bd79c6c1f830ca88ad8556a772e44
Dextet0526 <soul9862@naver.com>
2019-10-14 11:38:31 +0900

    no message

--- java1008/src/com/exam/PersonVO.java
+++ java1008/src/com/exam/PersonVO.java
@@ -1,45 +1,35 @@
 package com.exam;
 
 public class PersonVO {
-	private int id;
-	private String name;
-	private String company;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCompany() {
-		return company;
-	}
-	public void setCompany(String company) {
-		this.company = company;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public PersonVO(int id, String name, String company) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.company = company;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("personVO [id=").append(id).append(", name=").append(name).append(", company=").append(company)
-				.append("]");
-		return builder.toString();
-	}
-	public PersonVO() {
-		// TODO Auto-generated constructor stub
-	}
-	
+   private int id;
+   private String name;
+   private String company;
+   
+   public int getId() {
+      return id;
+   }
+   public void setId(int id) {
+      this.id = id;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getCompany() {
+      return company;
+   }
+   public void setCompany(String company) {
+      this.company = company;
+   }
+   
+   @Override
+   public String toString() {
+      StringBuilder builder = new StringBuilder();
+      builder.append("PersonVO [id=").append(id).append(", name=").append(name).append(", company=").append(company)
+            .append("]");
+      return builder.toString();
+   }
+   
 }
