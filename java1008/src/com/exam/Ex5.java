018e8177720c44c1a6124410e85ca9043fbc7b9a
Dextet0526 <soul9862@naver.com>
2019-10-14 11:37:54 +0900

    xml 파싱 실습

--- java1008/src/com/exam/Ex5.java
+++ java1008/src/com/exam/Ex5.java
@@ -1,6 +1,5 @@
 package com.exam;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,78 +10,76 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
+import org.w3c.dom.Node; 
 import org.w3c.dom.NodeList;
 
 public class Ex5 {
 
-	public static void main(String[] args) {
-		List<PersonVO> personList = new ArrayList<PersonVO>();
-		
-		// XML 파서 종류 2가지 : DOM 파서, SAX 파서
-		
-		
-		try {
-			URL url = Ex5.class.getResource("personList.xml");
-			InputStream is = url.openStream();
-			
-			// DOM 파서로 파싱하기
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(is); // 파싱수행 후 결과로 Document 객체 리턴
-			
-			System.out.println("파싱 후 Document 객체 생성완료!");
-			
-			
-			// 레코드 단위인 person 태그이름 기준으로 노드리스트로 가져오기
-			NodeList nodeList = document.getElementsByTagName("person");
-			
-			for (int i=0; i<nodeList.getLength(); i++) {
-				PersonVO personVO = new PersonVO();
-				Node node = nodeList.item(i); // person노드 한개씩 가져옴
-				
-				// 속성(attribute) 가져오기
-				NamedNodeMap nodeMap = node.getAttributes();
-				for (int j=0; j<nodeMap.getLength(); j++) {
-					Node attrNode = nodeMap.item(j);
-					if (attrNode.getNodeName().equals("id")) {
-						String id = attrNode.getNodeValue();
-						personVO.setId(Integer.parseInt(id));
-					}
-				} // inner for
-				
-				
-				// person노드의 자식요소들을 가져오기
-				NodeList childNodeList = node.getChildNodes();
-				
-				// 자식요소의 텍스트값을 찾기
-				for (int j=0; j<childNodeList.getLength(); j++) {
-					Node childNode = childNodeList.item(j);
-					
-					if (childNode.getNodeName().equals("name")) {
-						// String name = childNode.getChildNodes().item(0).getNodeValue();
-						String name = childNode.getFirstChild().getNodeValue();
-						personVO.setName(name);
-					} else if (childNode.getNodeName().equals("company")) {
-						String company = childNode.getFirstChild().getNodeValue();
-						personVO.setCompany(company);
-					}
-				} // inner for
-				
-				// VO 세팅이 끝나면 리스트에 추가
-				personList.add(personVO);
-			} // outer for
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("personList 생성 완료!");
-		
-		for (PersonVO personVO : personList) {
-			System.out.println(personVO); // 출력
-		}
-
-	} // main method
+   public static void main(String[] args) {
+      List<PersonVO> personList = new ArrayList<PersonVO>();
+      
+      // XML 파서 종류 2가지 : DOM 파서, SAX 파서
+      
+      
+      try {
+         URL url = Ex5.class.getResource("personList.xml");
+         InputStream is = url.openStream();
+         
+         // DOM 파서로 파싱하기
+         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+         DocumentBuilder builder = factory.newDocumentBuilder();
+         Document document = builder.parse(is); // 파싱수행 후 결과로 Document 객체 리턴
+         
+         System.out.println("파싱 후 Document 객체 생성완료!");
+         
+         // 레코드 단위인 person 태그이름 기준으로 노드리스트로 가져오기
+         NodeList nodeList = document.getElementsByTagName("person");
+         
+         for (int i=0; i<nodeList.getLength(); i++) {
+            PersonVO personVO = new PersonVO();
+            Node node = nodeList.item(i); // person노드 한개씩 가져옴
+            
+            // 속성(attribute) 가져오기
+            NamedNodeMap nodeMap = node.getAttributes();
+            for (int j=0; j<nodeMap.getLength(); j++) {
+               Node attrNode = nodeMap.item(j);
+               if (attrNode.getNodeName().equals("id")) {
+                  String id = attrNode.getNodeValue();
+                  personVO.setId(Integer.parseInt(id));
+               }
+            } // inner for
+                                     
+            // person노드의 자식요소들을 가져오기
+            NodeList childNodeList = node.getChildNodes();
+            
+            // 자식요소의 텍스트값을 찾기
+            for(int j=0; j<childNodeList.getLength(); j++) {
+               Node childNode = childNodeList.item(j);
+            
+               if(childNode.getNodeName().equals("name")) {
+                  //String name = childNode.getChildNodes().item(0)getNodeValue();
+                  String name = childNode.getFirstChild().getNodeValue();
+                  personVO.setName(name);
+               } else if (childNode.getNodeName().equals("company")) {
+                  String company = childNode.getFirstChild().getNodeValue();
+                  personVO.setCompany(company);
+               }
+            } // inner for
+            personList.add(personVO);   
+         } // outer for
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      System.out.println("personList 생성 완료");
+      
+      for(PersonVO personVO : personList) {
+         System.out.println(personVO);
+      }
+      
+      
+      
+      
+      
+   } // main method
 
 }
